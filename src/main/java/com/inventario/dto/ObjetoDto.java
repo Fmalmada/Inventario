3ecd32a8b04c14ff4b9482078955262db9c7c91b
fmalmada <fmalmada@fi.uba.ar>
2024-01-11 23:41:01 -0300

    Dto

---
+++ src/main/java/com/inventario/dto/ObjetoDto.java
@@ -0,0 +1,13 @@
+package com.inventario.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ObjetoDto {
+    private String nombre;
+    private int cantidad;
+}
